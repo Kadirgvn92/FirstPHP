b7f47d95f1e4a8ea9d6173917cac7e5fd440e4f8
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-16 17:36:21 +0300

    variables numbers

--- php-dersleri/index.php
+++ php-dersleri/index.php
@@ -1,18 +1,19 @@
-<?php
-    $title = "Merhaba dünyalı";
-    ?>
+<?php 
+
+    $title = "Merhaba Dünya" ;
+    $ad = "Kadir";
+    $soyad = "Güven";
+    $yas = 32;
+
+    echo " ".$ad." ".$title." ".$ad." ".$yas;
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    $urunAdi = "IPhone 14";
+    $urunFiyat = 30000;
+    $kdvOrani = 0.18;
+
+    $sonuc = $urunAdi." ".$urunFiyat + ($urunFiyat * $kdvOrani);
+
+    echo $sonuc;
     
-    <h1><?php echo $title ?></h1>
-
-</body>
-</html>
+?>
---
+++ php-dersleri/numbers.php
@@ -0,0 +1,11 @@
+<?php 
+
+$sayi1 = 1;
+$sayi2 = 2;
+
+echo "sonuc".$sayi1 + $sayi2;
+echo "sonuc".$sayi2 - $sayi1;
+echo "sonuc".$sayi2 * $sayi1;
+
+
+?>
